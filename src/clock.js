c649eff26bce2aa8cec15b293892870217567886
sangwoong <sangwoong03@gmail.com>
2022-02-22 17:27:26 +0900

    [JS Project - from nomadcoder] JS Function #3
    Added function to show the weather where I am with weather API from "https://openweathermap.org/"
    
    Changed styles and I will add more function like todo list for my calendar and changable view after login.

--- src/clock.js
+++ src/clock.js
@@ -4,10 +4,9 @@ function clockNow() {
   const date = new Date();
   const hours = String(date.getHours()).padStart(2, "0");
   const minutes = String(date.getMinutes()).padStart(2, "0");
-  const seconds = String(date.getSeconds()).padStart(2, "0");
 
-  clock.innerHTML = `<h2> ${hours}시 : ${minutes}분 : ${seconds}초 </h2>`;
+  clock.innerHTML = `<h2 class="time"> ${hours} : ${minutes} </h2>`;
 }
 
 clockNow();
-setInterval(clockNow, 1000);
+setInterval(clockNow, 60000);
---
+++ src/weather.js
@@ -0,0 +1,32 @@
+const API_KEY = "df91fcabb4316a128d6c4a8e9bce3877";
+
+function geoSuccess(position) {
+  const lat = position.coords.latitude;
+  const lng = position.coords.longitude;
+  const weatherUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&appid=${API_KEY}&units=metric`
+
+  fetch(weatherUrl)
+    .then(response => response.json())
+    .then(data => {
+      const climate = data.weather[0].main;
+      const site = data.name
+      const temperature = data.main.temp.toFixed(1);
+      
+      const weatherBox = document.querySelector(".weather-box");
+      weatherBox.innerHTML = 
+          `<p class="weahter--climate"> ${climate} </p>
+          <p class="weahter--place"> ${site} </p>
+          <p class="weahter--temperature"> ${temperature}도 </p>`
+    });
+};
+
+function createHtmlElement(data) {
+  return 
+}
+
+function geoFailure() {
+  console.log("error!!");
+};
+
+
+navigator.geolocation.getCurrentPosition(geoSuccess, geoFailure);
