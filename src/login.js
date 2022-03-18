82ac47afc3a8aaadb7a060e88b9011b41cddfcf1
sangwoong <sangwoong03@gmail.com>
2022-03-18 17:11:56 +0900

    [JS Project] JS Function For Login page

--- src/login.js
+++ src/login.js
@@ -1,42 +1,38 @@
-// 로그인 코드 구현
+import { USER_INFO } from "./modal.js";
+
+const loginForm = document.querySelector(".login-form");
 const userId = document.querySelector("#userid");
-const btnLogin = document.querySelector(".btn-login");
-const warningMsg = document.querySelector(".warning-msg");
+const userPw = document.querySelector("#userpw");
+const resetAll = document.querySelector(".reset");
+const Btn = document.querySelector(".login-btn");
 
-// save the user information in the localStorage
-let userInfo = [];
-const USER_NAME = "username";
+// 로컬스토리지 중 key: USER_INFO 리셋기능 추가하기
+function resetUserInfo() {
+  localStorage.clear();
+}
+function fetchUserInfo(name, pw) {
+  const savedInfos = localStorage.getItem(USER_INFO);
+  const parsedInfos = JSON.parse(savedInfos);
+  const newUserName = parsedInfos.map(a => a.username);
+  const newUserPw = parsedInfos.map(a => a.userpw);
 
-//submit the user's information to localStorage
-function submitName(name) {
-  const submitCheck = confirm(`${name}님! 이름을 등록하겠습니까?`);
-  
-  const date = new Date();
-  const year = date.getFullYear();
-  const month = date.getMonth();
-  const day = date.getDay();
+  const indexId = newUserName.indexOf(name);
+  const indexPw = newUserPw.indexOf(pw);
+  console.log(indexId, newUserPw);
 
-  const nameInfo = {
-    username: name,
-    useddate: `${year}년 ${month}월 ${day}일`,
-    id: userInfo.length + 1
-  };
-
-  if (submitCheck) {
-    userInfo.push(nameInfo);
-    saveUserName();
-    setTimeout(alert("등록이 완료되었습니다. \n다시 로그인 해주세요"), 500);
-  };
-};
-
-function saveUserName() {
-  localStorage.setItem(USER_NAME, JSON.stringify(userInfo));
-};
+  if ((indexId === -1 ) && (indexPw === -1 )) {
+    alert("회원 정보를 등록해주세요")
+  } else if ((indexId === -1 ) || (indexPw === -1 )) {
+    alert("아이디 혹은 비밀번호를 확인해주세요")
+  } else if ((indexId !== -1 ) && (indexPw !== -1 )) {
+    changeMainPage(name);
+  }
+}
 
 
 // if the user's information is in the localStorage, locate to todo-list page
 function changeMainPage(name) {
-  const textAlert = `${name}님 어제보다 나을 필요는 없습니다. \n오늘을, 지금 이 시간을 즐기세요!!`;
+  const textAlert = `${name}님 환영합니다`;
 
   alert(textAlert);
   if (alert) { location.href = "../todo/todo.html" };
@@ -46,25 +42,23 @@ function changeMainPage(name) {
 function handleLogin(e) {
   e.preventDefault();
   const name = userId.value;
+  const pw = userPw.value;
   userId.value = "";
+  userPw.value = "";
 
-  const savedInfos = localStorage.getItem(USER_NAME);
+  const savedInfos = localStorage.getItem(USER_INFO);
 
-  if (!name ) {
-    alert("이름을 입력해주세요");
-  } else if ((name && savedInfos === null)) {
-    submitName(name);
-  } else if ((name && savedInfos !== null)) {
-    const parsedInfos = JSON.parse(savedInfos);
-    userInfo = parsedInfos;
-    const newUserInfo = userInfo.map(a => a.username);
-    const index = newUserInfo.indexOf(name);
-    (index === -1 ) ? submitName(name) : changeMainPage(name);
+  if (!name || !pw ) {
+    alert("아이디와 비밀번호를 입력해주세요");
+  } else if ((name && pw && savedInfos === null)) {
+    alert("아이디와 비밀번호를 등록해주세요");
+  } else if ((name && pw && savedInfos !== null)) {
+    fetchUserInfo(name, pw);
   };
 };
 
-// 로컬스토리지 중 USER_INFO 리셋기능 추가하기
-function resetUserInfo() {
+function loginMain(){
+  loginForm.addEventListener("submit", handleLogin);
+  resetAll.addEventListener("click", resetUserInfo);
 }
-
-btnLogin.addEventListener("click", handleLogin);
+loginMain();
