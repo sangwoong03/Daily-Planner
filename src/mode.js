9ddd8550ed8643fa00dbb81fce883e9c8cf985d8
sangwoong <sangwoong03@gmail.com>
2022-03-23 21:37:47 +0900

    Javascript _ Mode of dark or light module

--- src/mode.js
+++ src/mode.js
@@ -1,34 +1,57 @@
-// 코드 구현
-const container = document.querySelector(".container");
-const isDark = document.querySelector(".dark-mode")
-const modeText = document.querySelector(".mode-text");
-const checkBox = document.querySelector(".dark-check");
-const todoBox = document.querySelector(".todos");
+const choiceMode = document.querySelector(".dark-mode");
+const textMode = document.querySelector(".mode-text")
+const body = document.querySelector("body");
+const box = document.querySelector(".box");
+const todoBox = document.querySelector(".container-todo");
+const clearBtn = document.querySelector(".clear-all");
+const todoUl = document.querySelector(".todo-list");
+const mobileBtn = document.querySelector(".mobile-submit");
+const submitImg = document.querySelector(".submit-img");
 
-isDark.onclick = () => {
-    isDark.checked ? dark() : light();
-    localStorage.setItem("currentmode", isDark.checked);
-};
 
-window.addEventListener('load', () => {
-    if (localStorage.getItem("currentmode")==="true") {
-        dark();
-        isDark.checked = true;
-    };
-});
+const MODE = "mode"
+
+function changeMode() {
+  choiceMode.checked ? light() : dark();
+  localStorage.setItem(MODE, choiceMode.checked);
+}
+
+function loadViewMode() {
+  const viewMode = localStorage.getItem(MODE);
+
+  if (viewMode === "false") {
+    dark();
+    choiceMode.checked = false;
+  } else {
+    light();
+    choiceMode.checked = true;
+  }
+}
+
+function light() { 
+  textMode.innerText = "라이트모드"
+  body.classList.add("light");
+  box.classList.add("light");
+  todoBox.classList.add("light");
+  clearBtn.classList.add("light");
+  todoUl.classList.add("light");
+  mobileBtn.classList.add("light");
+  submitImg.classList.add("light");
+}
 
 function dark() {
-    container.classList.add("dark");
-    checkBox.style.border = "1px solid #fff";
-    checkBox.setAttribute("data-key", "dark");
-    todoBox.style.border = "1px solid #fff";
-    modeText.innerHTML = "다크모드";
+  textMode.innerText = "다크모드"
+  body.classList.remove("light");
+  box.classList.remove("light");
+  todoBox.classList.remove("light");
+  clearBtn.classList.remove("light");
+  todoUl.classList.remove("light");
+  mobileBtn.classList.remove("light");
+  submitImg.classList.remove("light");
 }
 
-function light() {
-    container.classList.remove("dark");
-    checkBox.style.border = "1px solid #333";
-    checkBox.setAttribute("data-key", "light");
-    todoBox.style.border = "1px solid #333";
-    modeText.innerHTML = "라이트모드";
+function modeMain() {
+  loadViewMode();
+  choiceMode.addEventListener("change", changeMode);
 }
+modeMain();
